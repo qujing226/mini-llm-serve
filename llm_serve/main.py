a5bcc6d367fb2607cc115fc75bb146767fc0b230
qujing226 <qujing226@gmail.com>
2026-03-26 18:10:54 +0800

    feat(main): run python app

--- llm_serve/main.py
+++ llm_serve/main.py
@@ -4,23 +4,20 @@ import sys
 
 from connectrpc.request import RequestContext
 
-BASE_DIR = os.path.dirname(__file__)
-GEN_DIR = os.path.join(BASE_DIR, "gen")
-if GEN_DIR not in sys.path:
-    sys.path.insert(0, GEN_DIR)
-
 from mini_llm_serve.v1 import core_pb2
 from mini_llm_serve.v1.execute_pb2 import (
     ExecuteBatchResponse,
     ExecuteResult,
 )
 
-from mini_llm_serve.v1.execute_connect import(
+from mini_llm_serve.v1.execute_connect import (
     ExecuteService,
     ExecuteServiceASGIApplication,
 )
 
-class ExecutorServiceImpl(ExecutorService):
+
+
+class ExecuteServiceImpl(ExecuteService):
     async def execute_batch(self, request, ctx: RequestContext) -> ExecuteBatchResponse:
         reponse = ExecuteBatchResponse(
             batch_id = request.batch_id,
@@ -36,6 +33,8 @@ class ExecutorServiceImpl(ExecutorService):
                     task_id = item.task_id,
                 )
             )
+
+app = ExecuteServiceASGIApplication(ExecuteServiceImpl())
 
 def main():
     print("Hello from llm-serve!")
